79f9b5e71c7bae168d29b93be2aa4de7871258d6
nagakubo-motoki-6489 <420-student003@la-study.com>
2024-06-05 16:38:24 +0900

    first commit

---
+++ src/main/java/com/example/demo/TeamDevB3ShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamDevB3ShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamDevB3ShoppingApplication.class, args);
+	}
+
+}
