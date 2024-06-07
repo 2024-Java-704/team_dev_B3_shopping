c6a2166dbfc6a29afb067d28dfff1917f7123dd8
sakata-hiroki-6512 <420-student018@la-study.com>
2024-06-07 15:28:55 +0900

    Model

--- src/main/java/com/example/demo/model/CartItems.java
+++ src/main/java/com/example/demo/model/CartItems.java
@@ -1,5 +1,37 @@
 package com.example.demo.model;
 
 public class CartItems {
+	
+	private Integer id; //主キー
+	
+	private String title; //タイトル
+	
+	//デフォルトコンストラクタ
+	public CartItems() {
+		
+	}
+	
+	public CartItems(Integer id,String title) {
+	this.id=id;
+	this.title=title;
+	}
+	//ゲッター　セッター
+	public Integer getId() {
+		return id;
+	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	
 }
