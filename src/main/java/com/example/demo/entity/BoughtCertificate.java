341632e43582fc117edbc3e38c132c20dac77b8d
nagakubo-motoki-6489 <420-student003@la-study.com>
2024-06-07 14:20:54 +0900

    Entity

--- src/main/java/com/example/demo/entity/BoughtCertificate.java
+++ src/main/java/com/example/demo/entity/BoughtCertificate.java
@@ -1,5 +1,59 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "bought_certificate")
 public class BoughtCertificate {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private Integer saleListId;
+	
+	private LocalDateTime boughtDay;
+	
+	
+	public BoughtCertificate() {
+		
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public Integer getSaleListId() {
+		return saleListId;
+	}
+
+
+	public void setSaleListId(Integer saleListId) {
+		this.saleListId = saleListId;
+	}
+
+
+	public LocalDateTime getBoughtDay() {
+		return boughtDay;
+	}
+
+
+	public void setBoughtDay(LocalDateTime boughtDay) {
+		this.boughtDay = boughtDay;
+	}
+	
+	
 }
