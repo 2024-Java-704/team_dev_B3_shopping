6c55dd20e50ab3a46234d9efd4a7173b37537aff
nagakubo-motoki-6489 <166344270+nagakubo-motoki-6489@users.noreply.github.com>
2024-06-07 16:47:53 +0900

    Update Student.java

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="student")
+@Table(name="students")
 public class Student {
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
