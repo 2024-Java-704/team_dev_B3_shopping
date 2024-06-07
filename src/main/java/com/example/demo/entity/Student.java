d1e8f913d119d8f2dcc6f2c57d505f8f58e6f610
sakata-hiroki-6512 <420-student018@la-study.com>
2024-06-07 14:15:14 +0900

    Student(エンティティ)

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,5 +1,110 @@
 package com.example.demo.entity;
 
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student")
 public class Student {
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer id; //ユーザーID
+	
+	private String name;  //学生名
+	
+	private Date birth; //生年月日
+	
+	private String address; //住所
+	
+	private String email; //メールアドレス
+	
+	private String pass; //パスワード
+	
+	private String number; //学籍番号
+	
+	private String bankAccount; //口座情報
+	
+	private Integer status; //ステータス
+	
+	public Student() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	
 
 }
