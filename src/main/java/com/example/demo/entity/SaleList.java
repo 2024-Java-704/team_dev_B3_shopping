8ae49df867ed3a3d9a427a8e56ad74c01aa56ee9
taihei-mochizuki-6883 <420-student073@la-study.com>
2024-06-07 15:13:29 +0900

    Entity, Repository

--- src/main/java/com/example/demo/entity/SaleList.java
+++ src/main/java/com/example/demo/entity/SaleList.java
@@ -1,5 +1,39 @@
 package com.example.demo.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "sale_list")
 public class SaleList {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "student_id")
+	private Integer studentId;
+	
+	@Column(name = "bookinfo_id")
+	private Integer bookInfo;
+	
+	@Column(name = "sale_day")
+	private LocalDate saleDay;
+	
+	@Column(name = "item_status")
+	private Integer itemStatus;
+	
+	@Column(name = "sale_method")
+	private Integer saleMethod;
+	
+	//デフォルトコンストラクタ
+	public SaleList() {
+		
+	}
 }
