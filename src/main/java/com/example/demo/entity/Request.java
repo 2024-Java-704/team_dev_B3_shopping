411566a64ef8221c2e3fe8ac43bb3a857e8ec0f2
nagakubo-motoki-6489 <420-student003@la-study.com>
2024-06-07 14:30:09 +0900

    長久保担当箇所

--- src/main/java/com/example/demo/entity/Request.java
+++ src/main/java/com/example/demo/entity/Request.java
@@ -1,5 +1,23 @@
 package com.example.demo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "request")
 public class Request {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name = "student_id")
+	private Integer studentId;
+	
+	@Column(name = "bookinfo_id")
+	private Integer bookinfoId;
 }
