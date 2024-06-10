bd96a5c2d3631499e43be515cb98d0ae27c05f65
nagakubo-motoki-6489 <420-student003@la-study.com>
2024-06-10 10:11:44 +0900

    長久保　開発途中

--- src/main/java/com/example/demo/repository/SaleListRepository.java
+++ src/main/java/com/example/demo/repository/SaleListRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.SaleList;
 
 public interface SaleListRepository extends JpaRepository<SaleList, Integer>{
 
+	List<SaleList> findByStudentIdIs(Integer studentId);
+	
+	List<SaleList> findByItemStatusIs(Integer itemStatus);
 }
