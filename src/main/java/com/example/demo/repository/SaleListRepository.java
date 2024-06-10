8e2828191d8c80505f4622384e9022c770cc6db7
taihei-mochizuki-6883 <420-student073@la-study.com>
2024-06-10 15:14:44 +0900

    商品詳細画面作成。

--- src/main/java/com/example/demo/repository/SaleListRepository.java
+++ src/main/java/com/example/demo/repository/SaleListRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.SaleList;
 
 public interface SaleListRepository extends JpaRepository<SaleList, Integer>{
+	List<SaleList> findByItemStatus(Integer Status);
 
 }
