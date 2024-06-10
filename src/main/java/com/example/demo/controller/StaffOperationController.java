100e66ac304cf3aeb861e75551c96bd0e8d37e81
watanabe-tomoya-6752 <420-student001@la-study.com>
2024-06-10 15:40:31 +0900

    コントローラ作成

--- src/main/java/com/example/demo/controller/StaffOperationController.java
+++ src/main/java/com/example/demo/controller/StaffOperationController.java
@@ -1,5 +1,67 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class StaffOperationController {
 
+	@GetMapping("/")
+	public String accountAccess() {
+		return "";
+	}
+
+	@GetMapping("/")
+	public String accountDetail() {
+		return "";
+	}
+
+	@GetMapping("/")
+	public String accountAddAccess() {
+		return "";
+	}
+
+	@GetMapping("/")
+	public String accountAddDetail() {
+		return "";
+	}
+
+	@PostMapping("/")
+	public String accountAdd() {
+		return "";
+	}
+
+	@GetMapping("/")
+	public String accountRejectAccess() {
+		return "";
+	}
+
+	@PostMapping("/")
+	public String accountReject() {
+		return "";
+	}
+
+	@GetMapping("/")
+	public String accountUpdateAccess() {
+		return "";
+	}
+
+	@PostMapping("/")
+	public String accountUpdate() {
+		return "";
+	}
+
+	@GetMapping("/")
+	public String accountUpdateConfirm() {
+		return "";
+	}
+
+	@GetMapping("/")
+	public String accountBanConfirm() {
+		return "";
+	}
+
+	@PostMapping("/")
+	public String accountBan() {
+		return "";
+	}
 }
