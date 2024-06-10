07a8bc61ed9d151b9a8ac32fe76cca381cb15d8b
watanabe-tomoya-6752 <420-student001@la-study.com>
2024-06-10 14:18:34 +0900

    コントローラ作成

--- src/main/java/com/example/demo/controller/StaffPutUpController.java
+++ src/main/java/com/example/demo/controller/StaffPutUpController.java
@@ -1,5 +1,43 @@
 package com.example.demo.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 public class StaffPutUpController {
 
+	//出品申請一覧画面を表示する。
+	@GetMapping("/itemrequest")
+	public String putUpAccess() {
+		return "";
+	}
+
+	//出品申請詳細画面を表示する。
+	@GetMapping("/itemrequest/{id}/detail")
+	public String putUpDetail() {
+		return "";
+	}
+
+	//出品申請確認画面を表示する。
+	@PostMapping("/itemrequest/{id}/approval")
+	public String putUpConfirm() {
+		return "";
+	}
+
+	//出品申請処理を行い、完了画面を表示する。
+	@PostMapping("/itemrequest/{id}/approval")
+	public String putUp() {
+		return "";
+	}
+
+	//出品申請却下確認画面を表示する。
+	@PostMapping("/itemrequest/{id}/rejected")
+	public String putUpRejectConfirm() {
+		return "";
+	}
+
+	//出品申請却下処理を行い、完了画面を表示する。
+	@PostMapping("/itemrequest/{id}/rejected")
+	public String putUpReject() {
+		return "";
+	}
 }
