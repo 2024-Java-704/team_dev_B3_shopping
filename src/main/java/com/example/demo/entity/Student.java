208d1fc9df3ebee8f97c962b9e6d29f210354610
sakata-hiroki-6512 <420-student018@la-study.com>
2024-06-11 17:46:03 +0900

    Entity編集

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -46,6 +46,21 @@ public class Student {
 	public Student() {
 	}
 
+	public Student(String name, String number, String address, Date birth, String pass, String email, Integer status) {
+		this.name = name;
+		this.number = number;
+		this.address = address;
+		this.birth = birth;
+		this.pass = pass;
+		this.email = email;
+		this.status = status;
+
+	}
+
+	public Student(Integer status) {
+		this.status = status;
+	}
+
 	//ゲッター セッター
 	public Integer getId() {
 		return id;
@@ -118,20 +133,4 @@ public class Student {
 	public void setStatus(Integer status) {
 		this.status = status;
 	}
-
-	public Student(String name, String number, String address, Date birth, String pass, String email, Integer status) {
-		this.name = name;
-		this.number = number;
-		this.address = address;
-		this.birth = birth;
-		this.pass = pass;
-		this.email = email;
-		this.status = status;
-
-	}
-
-	public Student(Integer status) {
-		this.status = status;
-	}
-
 }
