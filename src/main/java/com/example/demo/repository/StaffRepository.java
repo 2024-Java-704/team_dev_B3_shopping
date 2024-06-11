fa067dbece9119aad0409c78dbb903739f5a25ba
nagakubo-motoki-6489 <420-student003@la-study.com>
2024-06-11 12:00:00 +0900

    adminController完成

--- src/main/java/com/example/demo/repository/StaffRepository.java
+++ src/main/java/com/example/demo/repository/StaffRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.entity.Staff;
 
 public interface StaffRepository extends JpaRepository<Staff, Integer>{
 
+	List<Staff> findByStaffNumber(String staffNumber);
 }
