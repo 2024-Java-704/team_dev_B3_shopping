8d63046ad8e3777c70ff8b26dc0b1cf68f655e29
taihei-mochizuki-6883 <420-student073@la-study.com>
2024-06-12 19:11:46 +0900

    StaffFunctionController、PageViewController、修正。途中。

--- src/main/java/com/example/demo/controller/PageViewController.java
+++ src/main/java/com/example/demo/controller/PageViewController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,49 +24,63 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class PageViewController {
-	
+
 	@Autowired
 	HttpSession session;
-	
+
 	@Autowired
 	AccountAndCart accountAndCart;
-	
+
 	@Autowired
 	BookinfoRepository bookinfoRepository;
-	
+
 	@Autowired
 	SaleListRepository saleListRepository;
-	
+
 	@Autowired
 	BookmarkRepository bookmarkRepository;
-	
+
 	@Autowired
 	StudentRepository studentRepository;
-	
+
 	//商品一覧画面表示
 	@GetMapping("/items")
-	public String index(Model model) {
-		if(accountAndCart.getId() == null) {
+	public String index(
+			@RequestParam(name = "keyword", defaultValue = "") String keyword,
+			Model model) {
+		if (accountAndCart.getId() == null) {
 			return "redirect:/login";
 		}
+
 		List<SaleList> saleList = saleListRepository.findByItemStatus(1);
 		List<Bookinfo> books = new ArrayList<>();
-		for(SaleList item: saleList) {
-			Bookinfo sale = bookinfoRepository.findById(item.getId()).get();
-			books.add(sale);
+
+		if (keyword.equals("")) { //キーワードが入力されなかった場合
+			for (SaleList sale : saleList) {
+				Bookinfo bookinfo = bookinfoRepository.findById(sale.getBookInfoId()).get();
+				books.add(bookinfo);
+			}
+		} else { //キーワードが入力された場合
+			for (SaleList sale : saleList) {
+				List<Bookinfo> bookinfos = bookinfoRepository.findByIdAndTitleContaining(sale.getBookInfoId(), keyword);
+				if (!bookinfos.isEmpty()) {
+					books.add(bookinfos.get(0));
+				}
+			}
 		}
+
 		model.addAttribute("books", books);
-		
-		if(accountAndCart.getId() != null) {
-		Student student = studentRepository.findById(accountAndCart.getId()).get();
-		
-			if(student.getStatus() == 5) {
+
+		if (accountAndCart.getId() != null) {
+			Student student = studentRepository.findById(accountAndCart.getId()).get();
+
+			if (student.getStatus() == 5) {
 				model.addAttribute("deniedMessage", "申請が却下されました");
 			}
 		}
 		return "index";
 	}
-	
+
 	//商品詳細画面表示
 	@GetMapping("/items/detail")
 	public String detail(@RequestParam("id") Integer id, Model model) {
@@ -75,47 +88,47 @@ public class PageViewController {
 		model.addAttribute("book", book);
 		return "detail";
 	}
-	
+
 	//ブックマーク画面表示
 	@GetMapping("/bookmark")
 	public String bookMark(Model model) {
 		List<Bookmark> bookmark = bookmarkRepository.findByStudentId(accountAndCart.getId());
-		
+
 		List<SaleList> saleList = new ArrayList<>();
-		for(Bookmark book: bookmark) {
+		for (Bookmark book : bookmark) {
 			SaleList sale = saleListRepository.findById(book.getSalelistId()).get();
 			saleList.add(sale);
 		}
-		
+
 		List<Bookinfo> books = new ArrayList<>();
-		for(SaleList sale : saleList) {
+		for (SaleList sale : saleList) {
 			Bookinfo info = bookinfoRepository.findById(sale.getBookInfoId()).get();
 			books.add(info);
 		}
-		
+
 		model.addAttribute("books", books);
 		return "bookmark";
 	}
-	
+
 	//ブックマーク追加処理
 	@PostMapping("/bookmark/add")
 	public String bookMarkAdd(@RequestParam("id") Integer id) {
 		List<SaleList> item = saleListRepository.findByBookInfoId(id);
 		Integer itemId = item.get(0).getId();
-		
+
 		List<Bookmark> bookmark = bookmarkRepository.findAll();
-		for(Bookmark book : bookmark) {
-			if(itemId == book.getSalelistId()) {
+		for (Bookmark book : bookmark) {
+			if (itemId == book.getSalelistId()) {
 				return "redirect:/bookmark";
 			}
 		}
-		
+
 		Integer accountId = accountAndCart.getId();
 		Bookmark book = new Bookmark(accountId, item.get(0).getId());
 		bookmarkRepository.save(book);
 		return "redirect:/bookmark";
 	}
-	
+
 	//マイページ画面表示
 	@GetMapping("/mypage")
 	public String mypage(Model model) {
