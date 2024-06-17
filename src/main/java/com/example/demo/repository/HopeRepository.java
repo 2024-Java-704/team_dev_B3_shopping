ad86e1fe53130522eae9f1b33cd5b74027bc711e
taihei-mochizuki-6883 <420-student073@la-study.com>
2024-06-17 17:19:37 +0900

    リクエスト申請Controller骨子、Entity、Repository、html作成。

---
+++ src/main/java/com/example/demo/repository/HopeRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entity.Hope;
+
+public interface HopeRepository extends JpaRepository<Hope, Integer> {
+
+}
