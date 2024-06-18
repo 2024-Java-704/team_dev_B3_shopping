6cddaa4657957c0b9d4c3bf5d8c6aba5bdf61d29
Sitou-souki-6075 <420-student047@la-study.com>
2024-06-18 14:39:13 +0900

    エラー修正

--- src/main/java/com/example/demo/repository/HopeRepository.java
+++ src/main/java/com/example/demo/repository/HopeRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.Hope;
 
 public interface HopeRepository extends JpaRepository<Hope, Integer> {
-	List<Hope>findByStatus(Integer Status);
+	List<Hope> findByStatus(Integer Status);
 
-	List<Hope> findByStudentIdAndTitle(Integer id, String title)
+	List<Hope> findByStudentIdAndTitle(Integer id, String title);
 
 	boolean existsByStudentIdAndTitle(Integer id, String title);
 }
